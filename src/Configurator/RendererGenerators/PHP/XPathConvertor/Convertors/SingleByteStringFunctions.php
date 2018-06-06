13c9f8fedef67f4e4f21fd71be05287df279e29e
JoshyPHP <s9e.dev@gmail.com>
2018-06-07 00:26:44 +0200

    WiP

--- src/Configurator/RendererGenerators/PHP/XPathConvertor/Convertors/SingleByteStringFunctions.php
+++ src/Configurator/RendererGenerators/PHP/XPathConvertor/Convertors/SingleByteStringFunctions.php
@@ -16,7 +16,9 @@ class SingleByteStringFunctions extends AbstractConvertor
 	{
 		return [
 			'Contains'      => 'Boolean',
+			'EndsWith'      => 'Boolean',
 			'NotContains'   => 'Boolean',
+			'NotEndsWith'   => 'Boolean',
 			'NotStartsWith' => 'Boolean',
 			'StartsWith'    => 'Boolean',
 			'StringLength'  => 'Number'
@@ -30,7 +32,9 @@ class SingleByteStringFunctions extends AbstractConvertor
 	{
 		return [
 			'Contains'      => 'contains \\( ((?&String)) , ((?&String)) \\)',
+			'EndsWith'      => 'ends-with \\( ((?&String)) , ((?&String)) \\)',
 			'NotContains'   => 'not \\( contains \\( ((?&String)) , ((?&String)) \\) \\)',
+			'NotEndsWith'   => 'not \\( ends-with \\( ((?&String)) , ((?&String)) \\) \\)',
 			'NotStartsWith' => 'not \\( starts-with \\( ((?&String)) , ((?&String)) \\) \\)',
 			'StartsWith'    => 'starts-with \\( ((?&String)) , ((?&String)) \\)',
 			'StringLength'  => 'string-length \\( ((?&String))? \\)'
@@ -46,7 +50,19 @@ class SingleByteStringFunctions extends AbstractConvertor
 	*/
 	public function convertContains($haystack, $needle)
 	{
-		return '(strpos(' . $this->convert($haystack) . ',' . $this->convert($needle) . ')!==false)';
+		return $this->generateContains($haystack, $needle, true);
+	}
+
+	/**
+	* Convert a call to ends-with()
+	*
+	* @param  string $string    Expression for the string part of the call
+	* @param  string $substring Expression for the substring part of the call
+	* @return string
+	*/
+	public function convertEndsWith($string, $substring)
+	{
+		return $this->generateEndsWith($string, $substring, true);
 	}
 
 	/**
@@ -58,7 +74,19 @@ class SingleByteStringFunctions extends AbstractConvertor
 	*/
 	public function convertNotContains($haystack, $needle)
 	{
-		return '(strpos(' . $this->convert($haystack) . ',' . $this->convert($needle) . ')===false)';
+		return $this->generateContains($haystack, $needle, false);
+	}
+
+	/**
+	* Convert a call to not(ends-with())
+	*
+	* @param  string $string    Expression for the string part of the call
+	* @param  string $substring Expression for the substring part of the call
+	* @return string
+	*/
+	public function convertNotEndsWith($string, $substring)
+	{
+		return $this->generateEndsWith($string, $substring, false);
 	}
 
 	/**
@@ -70,7 +98,7 @@ class SingleByteStringFunctions extends AbstractConvertor
 	*/
 	public function convertNotStartsWith($string, $substring)
 	{
-		return '(strpos(' . $this->convert($string) . ',' . $this->convert($substring) . ')!==0)';
+		return $this->generateStartsWith($string, $substring, false);
 	}
 
 	/**
@@ -82,7 +110,7 @@ class SingleByteStringFunctions extends AbstractConvertor
 	*/
 	public function convertStartsWith($string, $substring)
 	{
-		return '(strpos(' . $this->convert($string) . ',' . $this->convert($substring) . ')===0)';
+		return $this->generateStartsWith($string, $substring, true);
 	}
 
 	/**
@@ -94,5 +122,80 @@ class SingleByteStringFunctions extends AbstractConvertor
 	public function convertStringLength($expr = '.')
 	{
 		return "preg_match_all('(.)su'," . $this->convert($expr) . ')';
+	}
+
+	/**
+	* Generate the code for a call to contains()
+	*
+	* @param  string $haystack Expression for the haystack part of the call
+	* @param  string $needle   Expression for the needle part of the call
+	* @param  bool   $bool     Return value for a positive match
+	* @return string
+	*/
+	protected function generateContains($haystack, $needle, $bool)
+	{
+		$operator = ($bool) ? '!==' : '===';
+
+		return '(strpos(' . $this->convert($haystack) . ',' . $this->convert($needle) . ')' . $operator . 'false)';
+	}
+
+	/**
+	* Generate the code for a call to ends-with()
+	*
+	* @param  string $string    Expression for the string part of the call
+	* @param  string $substring Expression for the substring part of the call
+	* @param  bool   $bool      Return value for a positive match
+	* @return string
+	*/
+	protected function generateEndsWith($string, $substring, $bool)
+	{
+		return (preg_match('(^(?:\'[^\']+\'|"[^"]+")$)D', $substring))
+		     ? $this->generateEndsWithLiteral($string, $substring, $bool)
+		     : $this->generateEndsWithExpression($string, $substring, $bool);
+	}
+
+	/**
+	* Generate the code for a call to ends-with() where the second argument is a literal string
+	*
+	* @param  string $string    Expression for the string part of the call
+	* @param  string $substring Expression for a literal substring
+	* @param  bool   $bool      Return value for a positive match
+	* @return string
+	*/
+	protected function generateEndsWithLiteral($string, $substring, $bool)
+	{
+		$operator = ($bool) ? '===' : '!==';
+
+		return '(substr(' . $this->convert($string) . ',-' . (strlen($substring) - 2) . ')' . $operator . $this->convert($substring) . ')';
+	}
+
+	/**
+	* Generate the code for a call to ends-with()
+	*
+	* @param  string $string    Expression for the string part of the call
+	* @param  string $substring Expression for the substring part of the call
+	* @param  bool   $bool      Return value for a positive match
+	* @return string
+	*/
+	protected function generateEndsWithExpression($string, $substring, $bool)
+	{
+		$operator = ($bool) ? '' : '!';
+
+		return $operator . "preg_match('('.preg_quote(" . $this->convert($substring) . ").'$)D'," . $this->convert($string) . ')';
+	}
+
+	/**
+	* Generate the code for a call to starts-with()
+	*
+	* @param  string $string    Expression for the string part of the call
+	* @param  string $substring Expression for the substring part of the call
+	* @param  bool   $bool      Return value for a positive match
+	* @return string
+	*/
+	protected function generateStartsWith($string, $substring, $bool)
+	{
+		$operator = ($bool) ? '===' : '!==';
+
+		return '(strpos(' . $this->convert($string) . ',' . $this->convert($substring) . ')' . $operator . '0)';
 	}
 }
--- tests/Configurator/RendererGenerators/PHP/XPathConvertor/Convertors/SingleByteStringFunctionsTest.php
+++ tests/Configurator/RendererGenerators/PHP/XPathConvertor/Convertors/SingleByteStringFunctionsTest.php
@@ -17,10 +17,32 @@ class SingleByteStringFunctionsTest extends AbstractConvertorTest
 				'contains(@foo, @bar)',
 				"(strpos(\$node->getAttribute('foo'),\$node->getAttribute('bar'))!==false)"
 			],
+			// EndsWith
+			[
+				'ends-with(@foo, @bar)',
+				"preg_match('('.preg_quote(\$node->getAttribute('bar')).'\$)D',\$node->getAttribute('foo'))"
+			],
+			[
+				'ends-with(@foo, "/")',
+				"(substr(\$node->getAttribute('foo'),-1)==='/')"
+			],
+			[
+				"ends-with(@foo, '//')",
+				"(substr(\$node->getAttribute('foo'),-2)==='//')"
+			],
+			[
+				"ends-with(@foo, '😊')",
+				"(substr(\$node->getAttribute('foo'),-4)==='😊')"
+			],
 			// NotContains
 			[
 				'not(contains(@foo, @bar))',
 				"(strpos(\$node->getAttribute('foo'),\$node->getAttribute('bar'))===false)"
+			],
+			// NotEndsWith
+			[
+				"not(ends-with(@foo, '//'))",
+				"(substr(\$node->getAttribute('foo'),-2)!=='//')"
 			],
 			// NotStartsWith
 			[
